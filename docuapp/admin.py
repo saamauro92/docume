79e808e18e98b27a937e4a83c1d5869e5d32f321
saamauro92 <saa.mauro92@gmail.com>
2023-11-01 22:20:21 +0100

    Add summernote to docpost admin

--- docuapp/admin.py
+++ docuapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import DocPost
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+@admin.register(DocPost)
+
+class DocPostAdmin(SummernoteModelAdmin):
+    """
+    Defines the summernote field content in DocPost for the admin management
+    """
+    summernote_fields = ('content')
+
