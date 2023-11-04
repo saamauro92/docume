ce32fe5083bffd9c9a914ceadb1cc5d0e703c6b2
saamauro92 <saa.mauro92@gmail.com>
2023-11-04 17:36:18 +0100

    Create views and route for home page and explore page, set urls

--- docuapp/views.py
+++ docuapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import DocPost
 
-# Create your views here.
+class Home(generic.ListView): 
+    """
+    Creates home page view with docposts
+    """
+
+    model = DocPost
+    queryset = DocPost.objects.filter(status=1).order_by('-created_on')
+    template_name ='index.html'
+    paginate_by = 3
+
+class DocPostList(generic.ListView):
+    """
+    Creates explore page view with docposts
+    """
+    model = DocPost
+    queryset = DocPost.objects.filter(status=1).order_by('-created_on')
+    template_name = 'explore.html'
+    paginate_by = 6
