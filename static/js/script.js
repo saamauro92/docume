6af79682a72fa6b34ce47c3016274208f955571a
saamauro92 <saa.mauro92@gmail.com>
2023-11-15 00:04:59 +0100

    Add/fix logic for delete post that was deleted by mistake

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
 
+  /** This function will pop a JS prompt to ensure deleting or not 
+ * https://stackoverflow.com/questions/46625535/delete-confirmation-in-django */
+ function deletePost() {
+	var isValid = confirm('Are you sure you want to delete this post?');
+	if (!isValid) { event.preventDefault();}
+   }
+
+
 function toggleMenu() {
 	const navMenu = document.querySelector('.mainNavbar ul');
 	navMenu.classList.toggle('show');
   }
+
