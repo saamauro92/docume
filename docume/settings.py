bb838cf7c1650cdd61f41744abd2a742928da369
saamauro92 <saa.mauro92@gmail.com>
2023-11-18 13:20:18 +0100

    Add django messages and styles

--- docume/settings.py
+++ docume/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
     import env
 
@@ -63,6 +64,14 @@ CRISPY_TEMPLATE_PACK = 'uni-form'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
