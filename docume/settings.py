1320c304230b30fa5e6f6b7579f597c91c442a5f
saamauro92 <saa.mauro92@gmail.com>
2023-11-19 10:50:18 +0100

    DEPLOYMENT ISSUE WITH SASS: renaming files to test

--- docume/settings.py
+++ docume/settings.py
@@ -178,9 +178,9 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
  
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Sass configs
 STATICFILES_FINDERS = (
--- docume/urls.py
+++ docume/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('docuapp.urls'), name='docuapp_urls'),
     path("accounts/", include("allauth.urls")),
-
 ]
